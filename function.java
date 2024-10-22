02826af19de55c5f610460862507d6402735c28d
Kabila-datasirpi <kabila.m@datasirpi.com>
2024-10-22 17:08:30 +0530

    functions added

---
+++ function.java
@@ -0,0 +1,18 @@
+public class function {
+
+
+
+    void add(){
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+
+
+
+    public static void main(String[] args){
+        function obj1=new function();
+        obj1.add();
+
+    }
+}
